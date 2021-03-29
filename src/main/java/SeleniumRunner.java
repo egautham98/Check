6fcbef0c79ca9693980b0ee057f03ef3c3ed2064
egautham98 <68951697+egautham98@users.noreply.github.com>
2021-03-29 13:52:57 +0530

    Update SeleniumRunner.java

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -4,17 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumRunner {
 
 	public static void main(String [] args) {
 		// TODO Auto-generated method stub
-		 System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
-WebDriver driver = new ChromeDriver();
-
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
 		
-	    
+		 System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+	    WebDriver driver = new ChromeDriver(options);
 	     
 	    // Enter url.
 	    driver.get("http://www.linkedin.com/");
